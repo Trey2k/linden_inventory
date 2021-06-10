a2f2c133722c361cb846699adabafec9918346e5
Trey M <treym2k.dev@gmail.com>
2021-06-10 17:30:05 +0000

    added stuff

--- client/items.lua
+++ client/items.lua
@@ -7,7 +7,6 @@ end)
 AddEventHandler('linden_inventory:moneyshot', function()
 	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = 'You ate a Money Shot burger', length = 2500})
 	SetPlayerMaxArmour(PlayerId(), 150)
-	print(GetPlayerMaxArmour(PlayerId()))
 end)
 
 AddEventHandler('linden_inventory:water', function()
@@ -84,10 +83,7 @@ Citizen.CreateThread(function()
 	info.whisky = false
 	info.vodka  = false
 	local timer = 0
-	while true do 
-		if IsControlPressed(0, 179) then
-			print(GetPlayerMaxArmour(PlayerId()))
-		end
+	while true do
 		Citizen.Wait(1000)
 		if info.whisky then
 			timer = timer + 100
